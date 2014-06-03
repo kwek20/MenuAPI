5559fef22b08a51eb62b72b84292826b63b03a5a
brord van wierst <kwek20@gmail.com>
2014-06-03 03:05:01 +0200

    Started project

---
+++ menuapi/src/main/java/net/brord/plugins/menuapi/MenuAPI.java
@@ -0,0 +1,14 @@
+package net.brord.plugins.menuapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Project menuapi<br/>
+ * Class net.brord.plugins.menuapi.MenuAPI.java<br/>
+ * @author Brord
+ * @since 3 jun. 2014, 03:04:12
+ */
+public class MenuAPI extends JavaPlugin {
+
+	
+}
